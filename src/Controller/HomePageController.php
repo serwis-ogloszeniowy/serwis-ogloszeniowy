c9ed6f359c285562433d5359fc33546e65df0341
Mateusz Maciąg <mmaciag922@gmail.com>
2019-11-11 13:38:04 +0100

    Add htaccess and materialize files

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -8,10 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomePageController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="dupa")
      */
     public function index()
     {
+        $zmienna = 'witaj';
+
+        return $this->render('home.html.twig', ['zmienna' => $zmienna]);
+    }
+
+    /**
+     * @Route("/test", name="home")
+     */
+    public function test()
+    {
+
         $zmienna = 'witaj';
 
         return $this->render('home.html.twig', ['zmienna' => $zmienna]);
